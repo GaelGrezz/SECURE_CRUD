664213ac35638e795c8fc21e9e184b7e68a6527e
Gael Grezz <gaelgutierrezdev@gmail.com>
2026-02-16 15:18:48 -0500

    Fase 2: Implementar CRUD (CREATE, READ, UPDATE, DELETE) sobre la BD MySQL.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,2 @@
-import os 
-from dotenv import load_dotenv
-
-from core.db import test_sqlalchemy
-
-# todo: Se queda en main
-
 if __name__ == "__main__":
-    test_sqlalchemy()
+    print("Hola mundo")
